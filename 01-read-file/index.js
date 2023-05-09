421e8a3521d67c3a2d0ab90eff7ee5c35bc318f3
sten00777 <www.ep965@gmail.com>
2023-05-09 14:06:48 +0300

    fix: fix path in solution 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
-const { error } = require('console');
 const fs = require('fs');
-const readableStream = fs.createReadStream('text.txt', 'utf-8');
+const path = require('path');
+const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
 readableStream.on('data', (chunk) => console.log(chunk));
 readableStream.on('error', (error) => console.log(error.message));
