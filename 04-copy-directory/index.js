6ac597f375371b47778ef33849b186aa1d66f7de
sten00777 <www.ep965@gmail.com>
2023-05-09 07:26:14 +0300

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+    fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+      if (err) throw err;
+  });
+
+  fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, data) => {
+    data.forEach(elem =>  {
+      fs.copyFile(path.join(__dirname, 'files', elem.name), path.join(__dirname, 'files-copy', elem.name), (err) => {
+        if (err) throw err})
+    });
+  })
+}
+copyDir();
